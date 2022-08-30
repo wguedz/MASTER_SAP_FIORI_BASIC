2ff19ea06d0c34d44df7ddeba51552072e438cdc
Wilmer Guedez <wilmergdz.01@gmail.com>
2022-08-30 04:10:04 +0000

    lab_02_Estructura_Proyectos_SAPUI5 - Eliminar carpetas para creacion desde cero

--- sapui5/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["logaligroup/com/sapui5/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- sapui5/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"logaligroupcom/sapui5/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});
