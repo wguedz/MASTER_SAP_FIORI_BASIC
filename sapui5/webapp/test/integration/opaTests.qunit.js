0b3580b05448024a262a59630e854800b53a2f6f
Wilmer Guedez <wilmergdz.01@gmail.com>
2022-08-30 03:52:27 +0000

    lab_01_entorno_desarrollo

---
+++ sapui5/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["logaligroup/com/sapui5/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ sapui5/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"logaligroupcom/sapui5/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
